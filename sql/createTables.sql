9df975a589a3ff20b41a3ea7a453ff48fa061ea3
Alexander Gabriel <alex.barbalex@gmail.com>
2016-06-10 17:39:14 +0200

    create tables

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -6,12 +6,36 @@ CREATE TABLE ae.group (
 DROP TABLE IF EXISTS taxonomy;
 CREATE TABLE ae.taxonomy (
   "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  "name" text DEFAULT NULL,
+  "name" text NOT NULL,
   "description" text DEFAULT NULL,
   "links" text[] DEFAULT NULL,
   "lastUpdated" date DEFAULT NULL,
-  "organisationId" UUID DEFAULT NULL,
-  "groupId" text DEFAULT NULL,
+  "organizationId" UUID NOT NULL REFERENCES ae.organization (id),
+  "group" text DEFAULT NULL,
   "isGroupStandard" boolean DEFAULT FALSE
 );
 CREATE INDEX ON ae.taxonomy USING btree ("name");
+
+DROP TABLE IF EXISTS "taxObject";
+CREATE TABLE ae."taxObject" (
+  "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  "taxonomyId" UUID NOT NULL REFERENCES ae.taxonomy (id),
+  "objectId" UUID DEFAULT NULL REFERENCES ae.object (id),
+  "parentId" UUID DEFAULT NULL REFERENCES ae."taxObject" (id),
+  "name" text NOT NULL,
+  "objectProperties" jsonb DEFAULT NULL
+);
+
+DROP TABLE IF EXISTS object;
+CREATE TABLE ae.object (
+  "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  "group" text DEFAULT NULL REFERENCES ae.group (name),
+  "organizationId" UUID NOT NULL REFERENCES ae.organization (id)
+);
+
+DROP TABLE IF EXISTS "objectPropertyCollection";
+CREATE TABLE ae."objectPropertyCollection" (
+  "objectId" UUID DEFAULT NULL REFERENCES ae.object (id),
+  "propertyCollectionId" UUID NOT NULL REFERENCES ae.propertyCollection (id),
+  "properties" jsonb DEFAULT NULL
+);

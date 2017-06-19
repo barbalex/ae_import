3790bf21f2641aa9a59aa54ec3c3c445a497ad8b
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-19 22:26:20 +0200

    use async funtions

--- src/addUniqueNameConstraintToCollections.js
+++ src/addUniqueNameConstraintToCollections.js
@@ -1,17 +1,11 @@
 'use strict'
 
 module.exports = pgDb =>
-  new Promise((resolve, reject) => {
-    pgDb
-      .none('ALTER TABLE ae.property_collection ADD UNIQUE (name)')
-      .then(() =>
-        pgDb.none('ALTER TABLE ae.relation_collection ADD UNIQUE (name)')
-      )
-      .then(() => {
-        console.log('unique constraints added to collection names')
-        resolve()
-      })
-      .catch(error =>
-        reject(`error adding unique constraints to collection names: ${error}`)
-      )
-  })
+  pgDb
+    .none('ALTER TABLE ae.property_collection ADD UNIQUE (name)')
+    .then(() =>
+      pgDb.none('ALTER TABLE ae.relation_collection ADD UNIQUE (name)')
+    )
+    .then(() => {
+      console.log('unique constraints added to collection names')
+    })
---
+++ src/openDataTerms.js
@@ -0,0 +1,9 @@
+'use strict'
+
+export default `Open Data: Die veröffentlichten Daten dürfen mit
+Hinweis auf die Quelle vervielfältigt, verbreitet und weiter
+zugänglich gemacht, angereichert und bearbeitet sowie kommerziell
+genutzt werden. Für die Richtigkeit, Genauigkeit, Zuverlässigkeit und
+Vollständigkeit der bezogenen, ebenso wie der daraus erzeugten Daten
+und anderer mit Hilfe dieser Daten hergestellten Produkte wird
+indessen keine Haftung übernommen.`

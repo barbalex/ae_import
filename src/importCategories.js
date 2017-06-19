6f51ec5615a9bc6602e72483bd5ca472e6e4679c
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-19 22:55:09 +0200

    optimize

--- src/importCategories.js
+++ src/importCategories.js
@@ -3,9 +3,8 @@
 const categories = require('./categories')
 
 module.exports = async pgDb => {
-  const valueSql = categories.map(category => `('${category}')`).join(',')
-
   await pgDb.none('truncate ae.category cascade')
+  const valueSql = categories.map(category => `('${category}')`).join(',')
   await pgDb.none(`insert into ae.category (name) values ${valueSql};`)
   const cat = await pgDb.many('select * from ae.category')
   console.log(`${categories.length} categories imported`)

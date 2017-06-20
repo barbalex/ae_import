433fb7ff8a3d0d3b55bdea7d4f0d121fb7a159ac
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-20 14:32:16 +0200

    asyncify

--- src/importTaxonomiesNonLr.js
+++ src/importTaxonomiesNonLr.js
@@ -4,29 +4,21 @@ const _ = require('lodash')
 const uuidv1 = require('uuid/v1')
 const nonLrTaxonomies = require('./nonLrTaxonomies.js')
 
-module.exports = (pgDb, organizationId) =>
-  new Promise((resolve, reject) => {
-    nonLrTaxonomies.forEach(tax => {
-      tax.id = uuidv1()
-      tax.organization_id = organizationId
-    })
-    const fieldsSql = _.keys(nonLrTaxonomies[0]).join(',')
-    const valueSql = nonLrTaxonomies
-      .map(
-        tax => `('${_.values(tax).join("','").replace(/'',/g, 'null,')}')`
-      )
-      .join(',')
-    const sql = `
-    insert into
-      ae.taxonomy (${fieldsSql})
-    values
-      ${valueSql};`
-    pgDb
-      .none('truncate ae.taxonomy cascade')
-      .then(() => pgDb.none(sql))
-      .then(() => {
-        console.log(`${nonLrTaxonomies.length} nonLrTaxonomies imported`)
-        resolve(nonLrTaxonomies)
-      })
-      .catch(error => reject(`error importing nonLrTaxonomies ${error}`))
+module.exports = async (pgDb, organizationId) => {
+  nonLrTaxonomies.forEach(tax => {
+    tax.id = uuidv1()
+    tax.organization_id = organizationId
   })
+  const fieldsSql = _.keys(nonLrTaxonomies[0]).join(',')
+  const valueSql = nonLrTaxonomies
+    .map(tax => `('${_.values(tax).join("','").replace(/'',/g, 'null,')}')`)
+    .join(',')
+  await pgDb.none('truncate ae.taxonomy cascade')
+  await pgDb.none(`
+    insert into ae.taxonomy (${fieldsSql})
+    values ${valueSql};
+  `)
+  console.log(`${nonLrTaxonomies.length} nonLrTaxonomies imported`)
+
+  return nonLrTaxonomies
+}

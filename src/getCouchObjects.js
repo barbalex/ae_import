bdd54e602f15d1f7a855b2b69ca7104515b7973d
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-20 22:44:13 +0200

    correct error

--- src/getCouchObjects.js
+++ src/getCouchObjects.js
@@ -4,10 +4,6 @@ module.exports = async asyncCouchdbView => {
   const objekte = await asyncCouchdbView('artendb/objekte', {
     include_docs: true,
   })
-  // TODO: does this make sense????
-  // should probably extract doc, right?
-  console.log('getCouchObjects: objekte[0]:', objekte[0])
-  const objects = objekte.map(doc => doc)
-  console.log('getCouchObjects: objects[0]:', objects[0])
-  return objects
+
+  return objekte.map(obj => obj.doc)
 }

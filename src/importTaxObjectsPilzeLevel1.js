7ca2283ec28283b18c516fb798c16a11b89aac7f
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-20 14:23:44 +0200

    asyncify

--- src/importTaxObjectsPilzeLevel1.js
+++ src/importTaxObjectsPilzeLevel1.js
@@ -2,37 +2,27 @@
 
 const _ = require('lodash')
 const uuidv1 = require('uuid/v1')
+const { promisify } = require('util')
 
-module.exports = (couchDb, pgDb, taxPilze) =>
-  new Promise((resolve, reject) => {
-    couchDb.view(
-      'artendb/baumMacromycetes',
-      {
-        group_level: 1,
-      },
-      (error, result) => {
-        if (error) reject(`error querying view baumMacromycetes: ${error}`)
-        const names = _.map(result, row => row.key[0])
-        const taxObjectsPilzeLevel1 = names.map(name => ({
-          id: uuidv1(),
-          taxonomy_id: taxPilze.id,
-          name,
-        }))
-        const fieldsSql = _.keys(taxObjectsPilzeLevel1[0]).join(',')
-        const valueSql = taxObjectsPilzeLevel1
-          .map(
-            tax => `('${_.values(tax).join("','").replace(/'',/g, 'null,')}')`
-          )
-          .join(',')
-        const sql = `
-      insert into
-        ae.taxonomy_object (${fieldsSql})
-      values
-        ${valueSql};`
-        pgDb
-          .none(sql)
-          .then(() => resolve(taxObjectsPilzeLevel1))
-          .catch(err => reject(`error importing taxObjectsPilzeLevel1 ${err}`))
-      }
-    )
+module.exports = async (couchDb, pgDb, taxPilze) => {
+  const asyncCouchdbView = promisify(couchDb.view).bind(couchDb)
+  const baumMacromycetes = asyncCouchdbView('artendb/baumMacromycetes', {
+    group_level: 1,
   })
+  const names = _.map(baumMacromycetes, row => row.key[0])
+  const taxObjectsPilzeLevel1 = names.map(name => ({
+    id: uuidv1(),
+    taxonomy_id: taxPilze.id,
+    name,
+  }))
+  const fieldsSql = _.keys(taxObjectsPilzeLevel1[0]).join(',')
+  const valueSql = taxObjectsPilzeLevel1
+    .map(tax => `('${_.values(tax).join("','").replace(/'',/g, 'null,')}')`)
+    .join(',')
+  await pgDb.none(`
+    insert into ae.taxonomy_object (${fieldsSql})
+    values ${valueSql};
+  `)
+
+  return taxObjectsPilzeLevel1
+}

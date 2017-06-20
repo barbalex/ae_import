f016f5cef41e88020670ed7d1a30ea63874bbcfe
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-20 23:01:49 +0200

    drop roles before creating

--- sql/createDatabase.sql
+++ sql/createDatabase.sql
@@ -1,8 +1,11 @@
 CREATE DATABASE ae encoding 'UTF8';
 CREATE SCHEMA ae;
--- CREATE EXTENSION pgcrypto;
 CREATE EXTENSION "uuid-ossp";
+DROP ROLE IF EXISTS org_collection_writer;
 CREATE ROLE org_collection_writer;
+DROP ROLE IF EXISTS org_habitat_writer;
 CREATE ROLE org_habitat_writer;
+DROP ROLE IF EXISTS org_taxonomy_writer;
 CREATE ROLE org_taxonomy_writer;
+DROP ROLE IF EXISTS org_admin;
 CREATE ROLE org_admin;

14fd8c51a9888a8fea1807940248f11a773649cb
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-20 12:00:46 +0200

    asyncify

--- src/importTaxObjectsFaunaLevel3.js
+++ src/importTaxObjectsFaunaLevel3.js
@@ -2,57 +2,47 @@
 
 const _ = require('lodash')
 const uuidv1 = require('uuid/v1')
+const { promisify } = require('util')
 
-module.exports = (
+module.exports = async (
   couchDb,
   pgDb,
   taxFauna,
   taxObjectsFaunaLevel1,
   taxObjectsFaunaLevel2
-) =>
-  new Promise((resolve, reject) => {
-    couchDb.view(
-      'artendb/baumFauna',
-      {
-        group_level: 3,
-      },
-      (error, result) => {
-        if (error) reject(`error querying view baumFauna: ${error}`)
-        const keys = _.map(result, row => row.key)
-        const taxObjectsFaunaLevel3 = _.map(keys, key => {
-          const klasseObjektName = key[0]
-          const klasseObject = taxObjectsFaunaLevel1.find(
-            taxObj => taxObj.name === klasseObjektName
-          )
-          const ordnungName = key[1]
-          const ordnungObject = taxObjectsFaunaLevel2.find(
-            taxObj =>
-              taxObj.name === ordnungName &&
-              taxObj.parent_id === klasseObject.id
-          )
-          const name = key[2]
-          return {
-            id: uuidv1(),
-            taxonomy_id: taxFauna.id,
-            name,
-            parent_id: ordnungObject.id,
-          }
-        })
-        const fieldsSql = _.keys(taxObjectsFaunaLevel3[0]).join(',')
-        const valueSql = taxObjectsFaunaLevel3
-          .map(
-            tax => `('${_.values(tax).join("','").replace(/'',/g, 'null,')}')`
-          )
-          .join(',')
-        const sql = `
-      insert into
-        ae.taxonomy_object (${fieldsSql})
-      values
-        ${valueSql};`
-        pgDb
-          .none(sql)
-          .then(() => resolve(taxObjectsFaunaLevel3))
-          .catch(err => reject(`error importing taxObjectsFaunaLevel3 ${err}`))
-      }
-    )
+) => {
+  const asyncCouchdbView = promisify(couchDb.view)
+  const result = asyncCouchdbView('artendb/baumFauna', {
+    group_level: 3,
   })
+
+  const keys = _.map(result, row => row.key)
+  const taxObjectsFaunaLevel3 = _.map(keys, key => {
+    const klasseObjektName = key[0]
+    const klasseObject = taxObjectsFaunaLevel1.find(
+      taxObj => taxObj.name === klasseObjektName
+    )
+    const ordnungName = key[1]
+    const ordnungObject = taxObjectsFaunaLevel2.find(
+      taxObj =>
+        taxObj.name === ordnungName && taxObj.parent_id === klasseObject.id
+    )
+    const name = key[2]
+    return {
+      id: uuidv1(),
+      taxonomy_id: taxFauna.id,
+      name,
+      parent_id: ordnungObject.id,
+    }
+  })
+  const fieldsSql = _.keys(taxObjectsFaunaLevel3[0]).join(',')
+  const valueSql = taxObjectsFaunaLevel3
+    .map(tax => `('${_.values(tax).join("','").replace(/'',/g, 'null,')}')`)
+    .join(',')
+  await pgDb.none(`
+    insert into ae.taxonomy_object (${fieldsSql})
+    values ${valueSql};
+  `)
+
+  return taxObjectsFaunaLevel3
+}
--- src/importTaxObjectsFaunaLevel4.js
+++ src/importTaxObjectsFaunaLevel4.js
@@ -2,8 +2,9 @@
 
 const _ = require('lodash')
 const uuidv1 = require('uuid/v1')
+const { promisify } = require('util')
 
-module.exports = (
+module.exports = async (
   couchDb,
   pgDb,
   taxFauna,
@@ -11,77 +12,62 @@ module.exports = (
   taxObjectsFaunaLevel2,
   taxObjectsFaunaLevel3,
   couchObjects
-) =>
-  new Promise((resolve, reject) => {
-    couchDb.view(
-      'artendb/baumFauna',
-      {
-        group_level: 5,
-      },
-      (error, result) => {
-        if (error) reject(`error querying view baumFauna: ${error}`)
-        const keys = _.map(result, row => row.key)
-        const taxObjectsFaunaLevel4 = _.map(keys, key => {
-          const klasseObjektName = key[0]
-          const klasseObject = taxObjectsFaunaLevel1.find(
-            taxObj => taxObj.name === klasseObjektName
-          )
-          const ordnungObjektName = key[1]
-          const ordnungObject = taxObjectsFaunaLevel2.find(
-            taxObj =>
-              taxObj.name === ordnungObjektName &&
-              taxObj.parent_id === klasseObject.id
-          )
-          const familieName = key[2]
-          const familieObject = taxObjectsFaunaLevel3.find(
-            taxObj =>
-              taxObj.name === familieName &&
-              taxObj.parent_id === ordnungObject.id
-          )
-          const name = key[3].replace("'", '`')
-          const objId = key[4]
-          const object = couchObjects.find(obj => obj._id === objId)
-          const properties = object.Taxonomie.Eigenschaften
-          return {
-            id: uuidv1(),
-            taxonomy_id: taxFauna.id,
-            name,
-            object_id: objId,
-            properties,
-            parent_id: familieObject.id,
-          }
-        })
-        const valueSql = taxObjectsFaunaLevel4
-          .map(
-            val =>
-              `('${val.id}','${val.taxonomy_id}','${val.name}','${val.object_id}','${val.parent_id}')`
-          )
-          .join(',')
-        const sql = `
-      insert into
-        ae.taxonomy_object (id,taxonomy_id,name,object_id,parent_id)
-      values
-        ${valueSql};`
-
-        pgDb
-          .none(sql)
-          .then(() =>
-            Promise.all(
-              taxObjectsFaunaLevel4.map(val => {
-                const sql2 = `
-              UPDATE
-                ae.taxonomy_object
-              SET
-                properties = $1
-              WHERE
-                id = $2
-            `
-                return pgDb.none(sql2, [val.properties, val.id])
-              })
-            )
-          )
-          .then(() => resolve(taxObjectsFaunaLevel4))
-          .catch(err => reject(`error importing taxObjectsFaunaLevel4 ${err}`))
-      }
-    )
+) => {
+  const asyncCouchdbView = promisify(couchDb.view)
+  const result = asyncCouchdbView('artendb/baumFauna', {
+    group_level: 5,
   })
+
+  const keys = _.map(result, row => row.key)
+  const taxObjectsFaunaLevel4 = _.map(keys, key => {
+    const klasseObjektName = key[0]
+    const klasseObject = taxObjectsFaunaLevel1.find(
+      taxObj => taxObj.name === klasseObjektName
+    )
+    const ordnungObjektName = key[1]
+    const ordnungObject = taxObjectsFaunaLevel2.find(
+      taxObj =>
+        taxObj.name === ordnungObjektName &&
+        taxObj.parent_id === klasseObject.id
+    )
+    const familieName = key[2]
+    const familieObject = taxObjectsFaunaLevel3.find(
+      taxObj =>
+        taxObj.name === familieName && taxObj.parent_id === ordnungObject.id
+    )
+    const name = key[3].replace("'", '`')
+    const objId = key[4]
+    const object = couchObjects.find(obj => obj._id === objId)
+    const properties = object.Taxonomie.Eigenschaften
+    return {
+      id: uuidv1(),
+      taxonomy_id: taxFauna.id,
+      name,
+      object_id: objId,
+      properties,
+      parent_id: familieObject.id,
+    }
+  })
+  const valueSql = taxObjectsFaunaLevel4
+    .map(
+      val =>
+        `('${val.id}','${val.taxonomy_id}','${val.name}','${val.object_id}','${val.parent_id}')`
+    )
+    .join(',')
+  await pgDb.none(`
+    insert into ae.taxonomy_object (id,taxonomy_id,name,object_id,parent_id)
+    values ${valueSql};
+  `)
+  await Promise.all(
+    taxObjectsFaunaLevel4.map(val => {
+      const sql = `
+        UPDATE ae.taxonomy_object
+        SET properties = $1
+        WHERE id = $2
+      `
+      return pgDb.none(sql, [val.properties, val.id])
+    })
+  )
+
+  return taxObjectsFaunaLevel4
+}

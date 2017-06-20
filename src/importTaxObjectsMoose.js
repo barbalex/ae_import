7127bd941a8ed7c808af8db5b789c8cc262d183e
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-20 14:05:45 +0200

    asyncify

--- src/importTaxObjectsMoose.js
+++ src/importTaxObjectsMoose.js
@@ -5,54 +5,41 @@ const importTaxObjectsMooseLevel2 = require('./importTaxObjectsMooseLevel2.js')
 const importTaxObjectsMooseLevel3 = require('./importTaxObjectsMooseLevel3.js')
 const importTaxObjectsMooseLevel4 = require('./importTaxObjectsMooseLevel4.js')
 
-module.exports = (couchDb, pgDb, taxMoose, couchObjects) =>
-  new Promise((resolve, reject) => {
-    let taxObjectsMooseLevel1
-    let taxObjectsMooseLevel2
-    let taxObjectsMooseLevel3
-    let taxObjectsMooseLevel4
+module.exports = async (couchDb, pgDb, taxMoose, couchObjects) => {
+  const taxObjectsMooseLevel1 = await importTaxObjectsMooseLevel1(
+    couchDb,
+    pgDb,
+    taxMoose
+  )
+  const taxObjectsMooseLevel2 = await importTaxObjectsMooseLevel2(
+    couchDb,
+    pgDb,
+    taxMoose,
+    taxObjectsMooseLevel1
+  )
+  const taxObjectsMooseLevel3 = await importTaxObjectsMooseLevel3(
+    couchDb,
+    pgDb,
+    taxMoose,
+    taxObjectsMooseLevel1,
+    taxObjectsMooseLevel2
+  )
+  const taxObjectsMooseLevel4 = await importTaxObjectsMooseLevel4(
+    couchDb,
+    pgDb,
+    taxMoose,
+    taxObjectsMooseLevel1,
+    taxObjectsMooseLevel2,
+    taxObjectsMooseLevel3,
+    couchObjects
+  )
+  const taxObjectsMoose = [
+    ...taxObjectsMooseLevel1,
+    ...taxObjectsMooseLevel2,
+    ...taxObjectsMooseLevel3,
+    ...taxObjectsMooseLevel4,
+  ]
+  console.log(`${taxObjectsMoose.length} moose taxonomy objects imported`)
 
-    importTaxObjectsMooseLevel1(couchDb, pgDb, taxMoose)
-      .then(result => {
-        taxObjectsMooseLevel1 = result
-        return importTaxObjectsMooseLevel2(
-          couchDb,
-          pgDb,
-          taxMoose,
-          taxObjectsMooseLevel1
-        )
-      })
-      .then(result => {
-        taxObjectsMooseLevel2 = result
-        return importTaxObjectsMooseLevel3(
-          couchDb,
-          pgDb,
-          taxMoose,
-          taxObjectsMooseLevel1,
-          taxObjectsMooseLevel2
-        )
-      })
-      .then(result => {
-        taxObjectsMooseLevel3 = result
-        return importTaxObjectsMooseLevel4(
-          couchDb,
-          pgDb,
-          taxMoose,
-          taxObjectsMooseLevel1,
-          taxObjectsMooseLevel2,
-          taxObjectsMooseLevel3,
-          couchObjects
-        )
-      })
-      .then(result => {
-        taxObjectsMooseLevel4 = result
-        const taxObjectsMoose = taxObjectsMooseLevel1.concat(
-          taxObjectsMooseLevel2,
-          taxObjectsMooseLevel3,
-          taxObjectsMooseLevel4
-        )
-        console.log(`${taxObjectsMoose.length} moose taxonomy objects imported`)
-        resolve(taxObjectsMoose)
-      })
-      .catch(error => reject(error))
-  })
+  return taxObjectsMoose
+}

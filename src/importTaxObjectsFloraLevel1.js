77ff13257dbf100b46d765017f6fc5abc6083966
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-20 13:54:36 +0200

    asyncify

--- src/importTaxObjectsFloraLevel1.js
+++ src/importTaxObjectsFloraLevel1.js
@@ -2,37 +2,27 @@
 
 const _ = require('lodash')
 const uuidv1 = require('uuid/v1')
+const { promisify } = require('util')
 
-module.exports = (couchDb, pgDb, taxFlora) =>
-  new Promise((resolve, reject) => {
-    couchDb.view(
-      'artendb/baumFlora',
-      {
-        group_level: 1,
-      },
-      (error, result) => {
-        if (error) reject(`error querying view baumFlora: ${error}`)
-        const names = _.map(result, row => row.key[0])
-        const taxObjectsFloraLevel1 = names.map(name => ({
-          id: uuidv1(),
-          taxonomy_id: taxFlora.id,
-          name,
-        }))
-        const fieldsSql = _.keys(taxObjectsFloraLevel1[0]).join(',')
-        const valueSql = taxObjectsFloraLevel1
-          .map(
-            tax => `('${_.values(tax).join("','").replace(/'',/g, 'null,')}')`
-          )
-          .join(',')
-        const sql = `
-      insert into
-        ae.taxonomy_object (${fieldsSql})
-      values
-        ${valueSql};`
-        pgDb
-          .none(sql)
-          .then(() => resolve(taxObjectsFloraLevel1))
-          .catch(err => reject(`error importing taxObjectsFloraLevel1 ${err}`))
-      }
-    )
+module.exports = async (couchDb, pgDb, taxFlora) => {
+  const asyncCouchdbView = promisify(couchDb.view).bind(couchDb)
+  const baumFlora = asyncCouchdbView('artendb/baumFlora', {
+    group_level: 1,
   })
+  const names = _.map(baumFlora, row => row.key[0])
+  const taxObjectsFloraLevel1 = names.map(name => ({
+    id: uuidv1(),
+    taxonomy_id: taxFlora.id,
+    name,
+  }))
+  const fieldsSql = _.keys(taxObjectsFloraLevel1[0]).join(',')
+  const valueSql = taxObjectsFloraLevel1
+    .map(tax => `('${_.values(tax).join("','").replace(/'',/g, 'null,')}')`)
+    .join(',')
+  await pgDb.none(`
+    insert into ae.taxonomy_object (${fieldsSql})
+    values ${valueSql};
+  `)
+
+  return taxObjectsFloraLevel1
+}

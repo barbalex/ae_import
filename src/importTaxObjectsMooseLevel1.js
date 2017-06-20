3a021825461b51854635e48d2315ea0a006d3af3
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-20 14:09:49 +0200

    asyncify

--- src/importTaxObjectsMooseLevel1.js
+++ src/importTaxObjectsMooseLevel1.js
@@ -2,37 +2,27 @@
 
 const _ = require('lodash')
 const uuidv1 = require('uuid/v1')
+const { promisify } = require('util')
 
-module.exports = (couchDb, pgDb, taxMoose) =>
-  new Promise((resolve, reject) => {
-    couchDb.view(
-      'artendb/baumMoose',
-      {
-        group_level: 1,
-      },
-      (error, result) => {
-        if (error) reject(`error querying view baumMoose: ${error}`)
-        const names = _.map(result, row => row.key[0])
-        const taxObjectsMooseLevel1 = names.map(name => ({
-          id: uuidv1(),
-          taxonomy_id: taxMoose.id,
-          name,
-        }))
-        const fieldsSql = _.keys(taxObjectsMooseLevel1[0]).join(',')
-        const valueSql = taxObjectsMooseLevel1
-          .map(
-            tax => `('${_.values(tax).join("','").replace(/'',/g, 'null,')}')`
-          )
-          .join(',')
-        const sql = `
-      insert into
-        ae.taxonomy_object (${fieldsSql})
-      values
-        ${valueSql};`
-        pgDb
-          .none(sql)
-          .then(() => resolve(taxObjectsMooseLevel1))
-          .catch(err => reject(`error importing taxObjectsMooseLevel1 ${err}`))
-      }
-    )
+module.exports = async (couchDb, pgDb, taxMoose) => {
+  const asyncCouchdbView = promisify(couchDb.view).bind(couchDb)
+  const baumMoose = asyncCouchdbView('artendb/baumMoose', {
+    group_level: 1,
   })
+  const names = _.map(baumMoose, row => row.key[0])
+  const taxObjectsMooseLevel1 = names.map(name => ({
+    id: uuidv1(),
+    taxonomy_id: taxMoose.id,
+    name,
+  }))
+  const fieldsSql = _.keys(taxObjectsMooseLevel1[0]).join(',')
+  const valueSql = taxObjectsMooseLevel1
+    .map(tax => `('${_.values(tax).join("','").replace(/'',/g, 'null,')}')`)
+    .join(',')
+  await pgDb.none(`
+    insert into ae.taxonomy_object (${fieldsSql})
+    values ${valueSql};
+  `)
+
+  return taxObjectsMooseLevel1
+}

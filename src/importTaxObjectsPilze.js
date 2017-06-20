82edaa64e0877c4757f8ee15e47ce92b2ddd76ab
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-20 14:18:52 +0200

    asyncify

--- src/importTaxObjectsPilze.js
+++ src/importTaxObjectsPilze.js
@@ -3,29 +3,21 @@
 const importTaxObjectsPilzeLevel1 = require('./importTaxObjectsPilzeLevel1.js')
 const importTaxObjectsPilzeLevel2 = require('./importTaxObjectsPilzeLevel2.js')
 
-module.exports = (couchDb, pgDb, taxPilze, couchObjects) =>
-  new Promise((resolve, reject) => {
-    let taxObjectsPilzeLevel1
-    let taxObjectsPilzeLevel2
+module.exports = async (couchDb, pgDb, taxPilze, couchObjects) => {
+  const taxObjectsPilzeLevel1 = await importTaxObjectsPilzeLevel1(
+    couchDb,
+    pgDb,
+    taxPilze
+  )
+  const taxObjectsPilzeLevel2 = await importTaxObjectsPilzeLevel2(
+    couchDb,
+    pgDb,
+    taxPilze,
+    taxObjectsPilzeLevel1,
+    couchObjects
+  )
+  const taxObjectsPilze = [...taxObjectsPilzeLevel1, ...taxObjectsPilzeLevel2]
+  console.log(`${taxObjectsPilze.length} pilze taxonomy objects imported`)
 
-    importTaxObjectsPilzeLevel1(couchDb, pgDb, taxPilze)
-      .then(result => {
-        taxObjectsPilzeLevel1 = result
-        return importTaxObjectsPilzeLevel2(
-          couchDb,
-          pgDb,
-          taxPilze,
-          taxObjectsPilzeLevel1,
-          couchObjects
-        )
-      })
-      .then(result => {
-        taxObjectsPilzeLevel2 = result
-        const taxObjectsPilze = taxObjectsPilzeLevel1.concat(
-          taxObjectsPilzeLevel2
-        )
-        console.log(`${taxObjectsPilze.length} pilze taxonomy objects imported`)
-        resolve(taxObjectsPilze)
-      })
-      .catch(error => reject(error))
-  })
+  return taxObjectsPilze
+}

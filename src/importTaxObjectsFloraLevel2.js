4c40bd76fcb0a00c907744e49e960ef854623a01
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-20 13:57:11 +0200

    asyncify

--- src/importTaxObjectsFloraLevel2.js
+++ src/importTaxObjectsFloraLevel2.js
@@ -2,45 +2,35 @@
 
 const _ = require('lodash')
 const uuidv1 = require('uuid/v1')
+const { promisify } = require('util')
 
-module.exports = (couchDb, pgDb, taxFlora, taxObjectsFloraLevel1) =>
-  new Promise((resolve, reject) => {
-    couchDb.view(
-      'artendb/baumFlora',
-      {
-        group_level: 2,
-      },
-      (error, result) => {
-        if (error) reject(`error querying view baumFlora: ${error}`)
-        const keys = _.map(result, row => row.key)
-        const taxObjectsFloraLevel2 = _.map(keys, key => {
-          const familieName = key[0]
-          const familieObject = taxObjectsFloraLevel1.find(
-            taxObj => taxObj.name === familieName
-          )
-          const name = key[1]
-          return {
-            id: uuidv1(),
-            taxonomy_id: taxFlora.id,
-            name,
-            parent_id: familieObject.id,
-          }
-        })
-        const fieldsSql = _.keys(taxObjectsFloraLevel2[0]).join(',')
-        const valueSql = taxObjectsFloraLevel2
-          .map(
-            tax => `('${_.values(tax).join("','").replace(/'',/g, 'null,')}')`
-          )
-          .join(',')
-        const sql = `
-      insert into
-        ae.taxonomy_object (${fieldsSql})
-      values
-        ${valueSql};`
-        pgDb
-          .none(sql)
-          .then(() => resolve(taxObjectsFloraLevel2))
-          .catch(err => reject(`error importing taxObjectsFloraLevel2 ${err}`))
-      }
-    )
+module.exports = async (couchDb, pgDb, taxFlora, taxObjectsFloraLevel1) => {
+  const asyncCouchdbView = promisify(couchDb.view).bind(couchDb)
+  const result = asyncCouchdbView('artendb/baumFlora', {
+    group_level: 2,
   })
+  const keys = _.map(result, row => row.key)
+  const taxObjectsFloraLevel2 = _.map(keys, key => {
+    const familieName = key[0]
+    const familieObject = taxObjectsFloraLevel1.find(
+      taxObj => taxObj.name === familieName
+    )
+    const name = key[1]
+    return {
+      id: uuidv1(),
+      taxonomy_id: taxFlora.id,
+      name,
+      parent_id: familieObject.id,
+    }
+  })
+  const fieldsSql = _.keys(taxObjectsFloraLevel2[0]).join(',')
+  const valueSql = taxObjectsFloraLevel2
+    .map(tax => `('${_.values(tax).join("','").replace(/'',/g, 'null,')}')`)
+    .join(',')
+  await pgDb.none(`
+    insert into ae.taxonomy_object (${fieldsSql})
+    values ${valueSql};
+  `)
+
+  return taxObjectsFloraLevel2
+}

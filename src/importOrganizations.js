bcf11c6912ff97272da27f67eca2c5361e077678
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-20 11:07:28 +0200

    asyncify

--- src/importOrganizations.js
+++ src/importOrganizations.js
@@ -1,19 +1,12 @@
 'use strict'
 
-module.exports = pgDb =>
-  new Promise((resolve, reject) => {
-    const sql = `
-    insert into
-      ae.organization (name)
-    values
-      ('FNS Kt. ZH');`
-    pgDb
-      .none('truncate ae.organization cascade')
-      .then(() => pgDb.none(sql))
-      .then(() => pgDb.many('select * from ae.organization'))
-      .then(organizations => {
-        console.log('1 organization imported')
-        resolve(organizations)
-      })
-      .catch(error => reject(`error importing organizations ${error}`))
-  })
+module.exports = async pgDb => {
+  pgDb.none('truncate ae.organization cascade')
+  await pgDb.none(`
+    insert into ae.organization (name)
+    values ('FNS Kt. ZH');
+  `)
+  const organizations = await pgDb.many('select * from ae.organization')
+  console.log('1 organization imported')
+  return organizations
+}

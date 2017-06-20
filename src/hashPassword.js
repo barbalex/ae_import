39c746cee935673f231a7a92602514b969396b58
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-20 11:48:50 +0200

    promisify

--- src/hashPassword.js
+++ src/hashPassword.js
@@ -1,14 +1,14 @@
 'use strict'
 
 const bcrypt = require('bcrypt')
+const { promisify } = require('util')
 
-module.exports = password =>
-  new Promise((resolve, reject) => {
-    bcrypt.genSalt(10, (error, salt) => {
-      if (error) return reject(error)
-      bcrypt.hash(password, salt, (error2, hash) => {
-        if (error2) return reject(error2)
-        resolve(hash)
-      })
-    })
-  })
+const asyncGenSalt = promisify(bcrypt.genSalt)
+const asyncHash = promisify(bcrypt.hash)
+
+module.exports = async password => {
+  const salt = await asyncGenSalt(10)
+  const hash = await asyncHash(password, salt)
+
+  return hash
+}

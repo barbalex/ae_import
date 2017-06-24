fb15c3fc060195fc534e9a6b07545f3834a9aef1
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-24 11:19:37 +0200

    import synonyms both ways

--- src/importSynonymsFlora.js
+++ src/importSynonymsFlora.js
@@ -10,7 +10,7 @@ module.exports = async pgDb => {
     inner join ae.taxonomy on ae.taxonomy.id = ae.taxonomy_object.taxonomy_id
     where ae.taxonomy.name = 'SISF Index 2 (2005)' and properties->'Taxonomie ID' is not null;
   `)
-  const synonyms = floraSynonyms
+  const synonyms1 = floraSynonyms
     .map(s => {
       const id = idList.find(x => x.sisfnr === s.sisfnr)
       const idSynonym = idList.find(x => x.sisfnr === s.sisfnr_synonym)
@@ -24,6 +24,21 @@ module.exports = async pgDb => {
       return {}
     })
     .filter(x => x.taxonomy_object_id && x.taxonomy_object_id_synonym)
+  const synonyms2 = floraSynonyms
+    .map(s => {
+      const id = idList.find(x => x.sisfnr === s.sisfnr_synonym)
+      const idSynonym = idList.find(x => x.sisfnr === s.sisfnr)
+      if (id && id.id && idSynonym && idSynonym.id) {
+        return {
+          taxonomy_object_id: id.id,
+          taxonomy_object_id_synonym: idSynonym.id,
+        }
+      }
+      console.log('no id found for floraSynonyms:', s)
+      return {}
+    })
+    .filter(x => x.taxonomy_object_id && x.taxonomy_object_id_synonym)
+  const synonyms = [...synonyms1, ...synonyms2]
   const fieldsSql = _.keys(synonyms[0]).join(',')
   const valueSql = synonyms
     .map(tax => `('${_.values(tax).join("','").replace(/'',/g, 'null,')}')`)

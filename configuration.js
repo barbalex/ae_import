b17ddf182c3500d2c47cba8025adb5da1ce06b33
Alexander Gabriel <alex@gabriel-software.ch>
2017-06-26 22:53:36 +0200

    remove unused

--- configuration.js
+++ configuration.js
@@ -9,8 +9,6 @@ const pgPassfile = require('./pgDbPass.json')
 
 config.db = {}
 config.pg = {}
-config.pg.userName = pgPassfile.user
-config.pg.passWord = pgPassfile.pass
 config.pg.connectionString = `postgres://${pgPassfile.user}:${pgPassfile.pass}@localhost:5432/ae`
 
 module.exports = config

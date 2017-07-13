d209e65edddbac919e4b08823175cd3ffba97341
Alexander Gabriel <alex@gabriel-software.ch>
2017-07-13 17:26:44 +0200

    remove unneeded

--- src/importPropertyCollectionObjectsFromRC.js
+++ src/importPropertyCollectionObjectsFromRC.js
@@ -1,43 +1,9 @@
 'use strict'
 
-const extractRelationCollectionObjectsFromCouchObjects = require('./extractRelationCollectionObjectsFromCouchObjects')
+const extractRelationsFromCouchObjects = require('./extractRelationsFromCouchObjects')
 
 module.exports = async (pgDb, couchObjects) => {
-  const {
-    propertyCollectionObjects,
-    relations,
-  } = await extractRelationCollectionObjectsFromCouchObjects(couchObjects, pgDb)
-  // write propertyCollectionObjects
-  const valueSqlOPC = propertyCollectionObjects
-    .map(
-      val => `('${val.id}','${val.object_id}','${val.property_collection_id}')`
-    )
-    .join(',')
-  await pgDb.none(`
-    insert into ae.property_collection_object (id,object_id,property_collection_id)
-    values ${valueSqlOPC};
-  `)
-  await pgDb.tx(t =>
-    t.batch(
-      propertyCollectionObjects.map(val => {
-        const sql = `
-          UPDATE ae.property_collection_object
-          SET properties = $1
-          WHERE object_id = $2
-          AND property_collection_id = $3
-        `
-        return pgDb.none(sql, [
-          val.properties,
-          val.object_id,
-          val.property_collection_id,
-        ])
-      })
-    )
-  )
-  console.log(
-    `${propertyCollectionObjects.length} property collection objects imported from relation collections`
-  )
-
+  const relations = await extractRelationsFromCouchObjects(couchObjects, pgDb)
   // write relations
   const valueSql = relations
     .map(

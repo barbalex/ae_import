75de9318ee6c7f9ff98529fba8ac807a12f90fae
Alexander Gabriel <alex@gabriel-software.ch>
2017-07-15 17:31:48 +0200

    add categories_of_taxonomies_function

--- src/addFunctions.js
+++ src/addFunctions.js
@@ -123,5 +123,27 @@ module.exports = async pgDb => {
     ALTER FUNCTION ae.pco_properties_by_categories_function(categories text[])
       OWNER TO postgres;
   `)
+  await pgDb.none(`
+    CREATE OR REPLACE FUNCTION ae.categories_of_taxonomies_function()
+      RETURNS setof ae.categories_of_taxonomies AS
+      $$
+        WITH categoryTaxonomies AS (
+          SELECT ae.category.name, ae.category.id, ae.taxonomy.id AS taxonomy_id
+          FROM ae.taxonomy
+            INNER JOIN ae.object
+              INNER JOIN ae.category
+              ON ae.category.name = ae.object.category
+            ON ae.object.taxonomy_id = ae.taxonomy.id
+          GROUP BY ae.category.name, ae.category.id, ae.taxonomy.id
+        )
+        SELECT name, id, count(*) AS count
+        FROM categoryTaxonomies
+        GROUP BY name, id
+        ORDER BY name
+      $$
+      LANGUAGE sql STABLE;
+    ALTER FUNCTION ae.categories_of_taxonomies_function()
+      OWNER TO postgres;
+  `)
   console.log('functions added')
 }

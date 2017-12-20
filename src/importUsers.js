bf21cbb5f1316400f4974c41e724f39126e037e3
Alexander Gabriel <alex@gabriel-software.ch>
2017-12-20 15:25:28 +0100

    add users to auth, not ae

--- src/importUsers.js
+++ src/importUsers.js
@@ -6,12 +6,12 @@ module.exports = async pgDb => {
   const hash = await hashPassword('secret')
   await pgDb.none(`
     insert into
-      ae.user (name,email,password)
+      auth.user (id,name,email,role,pass)
     values
-      ('Alexander Gabriel', 'alex@gabriel-software.ch', '${hash}'),
-      ('Andreas Lienhard', 'andreas.lienhard@bd.zh.ch', '${hash}');
+      ('a8eeeaa2-696f-11e7-b454-83e34acbe09f', 'Alexander Gabriel', 'alex@gabriel-software.ch', 'org_admin', '${hash}'),
+      ('a8ef38f4-696f-11e7-b455-03a921a2ac8f', 'Andreas Lienhard', 'andreas.lienhard@bd.zh.ch', 'org_admin', '${hash}');
   `)
-  const users = await pgDb.many('select * from ae.user')
+  const users = await pgDb.many('select * from auth.user')
   console.log('2 users imported')
 
   return users

146ad12d9abc598bec5da84e9211f9c8415f5bb6
Alexander Gabriel <alex.barbalex@gmail.com>
2017-12-26 11:28:53 +0100

    correct error

--- src/importOrganizations.js
+++ src/importOrganizations.js
@@ -3,7 +3,7 @@
 module.exports = async pgDb => {
   await pgDb.none(`
     insert into ae.organization (name)
-    values ('a8e5bc98-696f-11e7-b453-3741aafa0388',FNS Kt. ZH');
+    values ('a8e5bc98-696f-11e7-b453-3741aafa0388','FNS Kt. ZH');
   `)
   const organizations = await pgDb.many('select * from ae.organization')
   console.log('1 organization imported')

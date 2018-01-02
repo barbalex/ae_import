88676f815b6699e9fa5cf204e7c17f2e321d0a83
Alexander Gabriel <alex.barbalex@gmail.com>
2018-01-02 10:15:25 +0100

    dont hyphen urls

--- src/importOrganizations.js
+++ src/importOrganizations.js
@@ -3,7 +3,7 @@
 module.exports = async pgDb => {
   await pgDb.none(`
     insert into ae.organization (id,name,links,contact)
-    values ('a8e5bc98-696f-11e7-b453-3741aafa0388','FNS Kt. ZH','{"http://www.naturschutz.zh.ch"}','a8ef38f4-696f-11e7-b455-03a921a2ac8f');
+    values ('a8e5bc98-696f-11e7-b453-3741aafa0388','FNS Kt. ZH','{http://www.naturschutz.zh.ch}','a8ef38f4-696f-11e7-b455-03a921a2ac8f');
   `)
   console.log('1 organization imported')
   return

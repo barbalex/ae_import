2b16eb07e57a66473d0fcff682ecc7bd7816ecad
Alexander Gabriel <alex.barbalex@gmail.com>
2018-01-25 21:15:14 +0100

    correct org users

--- src/importOrganizationUsers.js
+++ src/importOrganizationUsers.js
@@ -4,12 +4,8 @@ module.exports = async (pgDb, organizationId) => {
   await pgDb.none(`
     insert into ae.organization_user (organization_id,user_id,role)
     values
-      ('${organizationId}', 'a8eeeaa2-696f-11e7-b454-83e34acbe09f', 'orgHabitatWriter'),
-      ('${organizationId}', 'a8eeeaa2-696f-11e7-b454-83e34acbe09f', 'orgCollectionWriter'),
       ('${organizationId}', 'a8eeeaa2-696f-11e7-b454-83e34acbe09f', 'orgAdmin'),
-      ('${organizationId}', 'a8ef38f4-696f-11e7-b455-03a921a2ac8f', 'orgAdmin'),
-      ('${organizationId}', 'a8ef38f4-696f-11e7-b455-03a921a2ac8f', 'orgHabitatWriter'),
-      ('${organizationId}', 'a8ef38f4-696f-11e7-b455-03a921a2ac8f', 'orgCollectionWriter');
+      ('${organizationId}', 'a8ef38f4-696f-11e7-b455-03a921a2ac8f', 'orgAdmin');
   `)
-  console.log('6 organization_users imported')
+  console.log('2 organization_users imported')
 }

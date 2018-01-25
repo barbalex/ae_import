3bd1862380d4d5b1db87cc7e7a018610617d681e
Alexander Gabriel <alex.barbalex@gmail.com>
2018-01-25 21:24:06 +0100

    update function

--- src/addFunctions.js
+++ src/addFunctions.js
@@ -300,24 +300,16 @@ module.exports = async pgDb => {
   `)
   await pgDb.none(`
   CREATE OR REPLACE FUNCTION ae.categories_of_taxonomies_function()
-    RETURNS TABLE (
-      taxonomy_id UUID,
-      taxonomy_name TEXT,
-      category_name TEXT
-    ) AS
+    RETURNS setof ae.categories_of_taxonomies AS
     $$
       SELECT DISTINCT
         ae.taxonomy.id as taxonomy_id,
-        ae.taxonomy.name as taxonomy_name,
         ae.category.name as category_name
       FROM ae.taxonomy
         INNER JOIN ae.object
           INNER JOIN ae.category
           ON ae.category.name = ae.object.category
         ON ae.object.taxonomy_id = ae.taxonomy.id
-      ORDER BY
-        taxonomy_name,
-        category_name
     $$
     LANGUAGE sql STABLE;
   ALTER FUNCTION ae.categories_of_taxonomies_function()

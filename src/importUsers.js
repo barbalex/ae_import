99fd1e4c308843c7311c729d36d9bc1331da108e
Alexander Gabriel <alex.barbalex@gmail.com>
2018-01-25 21:13:26 +0100

    change schema

--- src/importUsers.js
+++ src/importUsers.js
@@ -11,7 +11,7 @@ module.exports = async pgDb => {
       ('a8eeeaa2-696f-11e7-b454-83e34acbe09f', 'Alexander Gabriel', 'alex@gabriel-software.ch', 'org_admin', '${hash}'),
       ('a8ef38f4-696f-11e7-b455-03a921a2ac8f', 'Andreas Lienhard', 'andreas.lienhard@bd.zh.ch', 'org_admin', '${hash}');
   `)
-  const users = await pgDb.many('select * from auth.user')
+  const users = await pgDb.many('select * from ae.user')
   console.log('2 users imported')
 
   return users

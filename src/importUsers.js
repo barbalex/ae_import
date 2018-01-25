20fe81c51b6581f06c0fd5cfc6b132765e763de6
Alexander Gabriel <alex.barbalex@gmail.com>
2018-01-25 21:12:05 +0100

    change schema

--- src/importUsers.js
+++ src/importUsers.js
@@ -6,7 +6,7 @@ module.exports = async pgDb => {
   const hash = await hashPassword('secret')
   await pgDb.none(`
     insert into
-      auth.user (id,name,email,role,pass)
+      ae.user (id,name,email,role,pass)
     values
       ('a8eeeaa2-696f-11e7-b454-83e34acbe09f', 'Alexander Gabriel', 'alex@gabriel-software.ch', 'org_admin', '${hash}'),
       ('a8ef38f4-696f-11e7-b455-03a921a2ac8f', 'Andreas Lienhard', 'andreas.lienhard@bd.zh.ch', 'org_admin', '${hash}');

67409ded18dce1405dd8f815a563b024bb1cf5a2
Alexander Gabriel <alex.barbalex@gmail.com>
2018-02-06 11:46:39 +0100

    correct sql

--- src/updateOriginOfPc.js
+++ src/updateOriginOfPc.js
@@ -2,14 +2,14 @@
 
 module.exports = async pgDb => {
   await pgDb.none(`
-    update ae.property_collection set pc_of_origin = pc2.id
-    from ae.property_collection pc1
-    inner join ae.property_collection pc2
-    on pc1.pc_of_origin_name = pc2.name
-    where pc1.pc_of_origin_name is not null;
+    update ae.property_collection as x
+    set pc_of_origin = y.id
+    from ae.property_collection as y
+    where
+      y.name = x.pc_of_origin_name;
   `)
   await pgDb.none(`
     alter table ae.property_collection drop pc_of_origin_name;
   `)
-  console.log(`converted origin_of_pc_name to origin_of_pc`)
+  console.log(`converted origin_of_pc_name to origin_of_pc (id)`)
 }
